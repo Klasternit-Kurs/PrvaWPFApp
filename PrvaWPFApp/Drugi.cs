963cbe56c2582a132bbba4de2bfa46f3af80a324
Dragan Pepic <d.pepic@hotmail.com>
2020-07-20 19:06:57 +0200

    Add project files.

---
+++ PrvaWPFApp/Drugi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrvaWPFApp
+{
+	partial class KlasaUViseFajlova
+	{
+		public KlasaUViseFajlova()
+		{
+			broj = 5;
+		}
+	}
+}
